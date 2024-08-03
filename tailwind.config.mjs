e1526547b6db29bcbb23ad3549acfa24856f5283
juandurefaccini <juanchidure@outlook.com>
2024-08-03 14:53:57 -0300

    Install tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#6CA6C4",
+      background: "#F0F6F9",
+      text: "#001011",
+    },
+  },
+  plugins: [],
+};
