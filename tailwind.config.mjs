de2ec7896cd8ac06de831c415ce45c1de2034a05
juandurefaccini <juanchidure@outlook.com>
2024-08-28 16:39:10 -0300

    Refactor prices components and adjust layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,13 +2,14 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
-    colors: {
-      primary: "#3AC265",
-      secondary: "#f4f4f5",
-      background: "#FFFAFA",
-      text: "#0A2915",
-      debug: "#FF0000",
+    extend: {
+      colors: {
+        primary: "#3AC265",
+        secondary: "#FFFAFA",
+        background: "#FFFAFA",
+        text: "#272727",
+        debug: "#FF0000",
+      },
     },
   },
   plugins: [],
