06c58e326dc7d30be6858372f4958a91920234b3
juandurefaccini <juanchidure@outlook.com>
2024-09-02 16:09:56 -0300

    Refactor Astro config to remove unused dependencies and optimize integration setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   experimental: {
     env: {
       schema: {
