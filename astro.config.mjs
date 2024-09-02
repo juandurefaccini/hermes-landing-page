dd7f09c9b2cbe30bda568da403964e7043c8b6bc
juandurefaccini <juanchidure@outlook.com>
2024-09-02 18:45:43 -0300

    Use node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel(),
+  adapter: node(
+    {
+      mode: "standalone",
+    }
+  ),
   experimental: {
     env: {
       schema: {
